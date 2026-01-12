b45adf4ad020d0e5efd9d346d079092177bf9b14
Robin <rehill012@gmail.com>
2026-01-12 12:49:39 -0500

    Minor comment tweak

--- source/replacement.js
+++ source/replacement.js
@@ -8,7 +8,7 @@ var ligatureLevel = "off";
 var dashing = "off";
 
 // =======================================================
-// TEXT MANIPULATION
+// TEXT REPLACEMENT
 // =======================================================
 
 /*
